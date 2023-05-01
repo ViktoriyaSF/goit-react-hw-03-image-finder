890c3d00127d91a6c33be0c8e10e3fc59ef8e1a3
ViktoriyaSF <kievstar01051985@gmail.com>
2023-05-01 22:53:54 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
 import { Layout } from './Layout/Layout';
 import { GlobalStyle } from './BasicStyles/GlobalStyle';
-import { Section } from './Section/Section';
+// import { Section } from './Section/Section';
 
 export class App extends Component {
-  state = {};
+  state = {
+    photoName: ' ',
+  };
+  handleInputSearch = evt => {
+    const { value } = evt.currentTarget;
+    this.setState({ photoName: value.trim() });
+    console.log(value);
+  };
+  handleSubmit = evt => {
+    evt.preventDefault();
+    this.setState({ photoName: ' ' }); //по чистили
+  };
+
   render() {
     return (
       <Layout>
-        <Section>React homework templatedddddd</Section>
+        <header>
+          <form on Submit={this.handleSubmit}>
+            <button type="submit">
+              <span>Search</span>
+            </button>
+
+            <input
+              // value={photoName}
+              type="text"
+              autocomplete="off"
+              autofocus
+              placeholder="Search images and photos"
+              onChange={this.handleInputSearch}
+            />
+          </form>
+        </header>
+        {/* <header class="searchbar">
+          <form class="form">
+            <button type="submit" class="button">
+              <span class="button-label">Search</span>
+            </button>
+
+            <input
+              class="input"
+              type="text"
+              autocomplete="off"
+              autofocus
+              placeholder="Search images and photos"
+            />
+          </form>
+        </header> */}
         <GlobalStyle />
       </Layout>
     );
